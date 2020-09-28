a22ff2eacdb29fcd0ff27fe6afc54cb767a02198
azharshahstl <azharshahstl@yahoo.com>
2020-09-28 15:59:01 -0500

    Added strong params to user and recipe

--- app/api/v1/controllers/recipes_controller.rb
+++ app/api/v1/controllers/recipes_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::RecipesController < ApplicationController
+
+    def index 
+    end 
+
+    def show 
+    end 
+
+    def new 
+    end 
+
+    def update 
+    end
+
+    def destroy 
+    end 
+
+    private 
+
+    def recipe_params 
+        params.require(:recipe).permit(:ingredients, :directions, :cuisine, :user_id)
+    end
+
 end
--- app/api/v1/controllers/users_controller.rb
+++ app/api/v1/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::UsersController < ApplicationController
+
+    def create 
+    end 
+
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:name, :email, :password)
+    end
+
+
 end
