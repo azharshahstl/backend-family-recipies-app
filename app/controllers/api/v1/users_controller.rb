160d72cc1bc31d1448b995492b21d62c130770f6
azharshahstl <azharshahstl@yahoo.com>
2020-10-02 17:08:54 -0500

    created controller actions and nested routes and controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,18 @@
 class Api::V1::UsersController < ApplicationController
 
+    def index 
+        users = User.all 
+        render json: users
+    end 
+    
+    
     def create 
         user = User.new(user_params)
         if user.valid? 
             render json: user 
         else 
-            render json {error: 'Either that email is taken or you did not fill in a name or password'}
+            render json: {error: 'Either that email is taken or you did not fill in a name or password'}
+        end
     end 
 
 
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email
-  has_many :recipies
+  has_many :recipes
 end
