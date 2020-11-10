b238bde1987b1c8cdf2c41dde58938a401aab751
azharshahstl <azharshahstl@yahoo.com>
2020-11-10 17:46:58 -0600

    removed / from origins in cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://nostalgic-minsky-e2fda9.netlify.app/'
+    origins 'https://nostalgic-minsky-e2fda9.netlify.app'
 
     resource '*',
       headers: :any,
