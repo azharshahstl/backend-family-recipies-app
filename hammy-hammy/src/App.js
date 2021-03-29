6eacc073203b0d956b4d887090f34caffe77eaa6
azharshahstl <azharshahstl@yahoo.com>
2021-03-29 16:52:38 -0500

    Added a comment to Layout.js

---
+++ hammy-hammy/src/App.js
@@ -0,0 +1,12 @@
+import classes from  './App.module.css';
+import React, { Component } from 'react'
+class App extends Component {
+  render() {
+  return (
+    <div className={classes.App}>
+      <p>hello there!</p>
+    </div>
+  )};
+}
+
+export default App;
---
+++ hammy-hammy/src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Aux from '../../hoc/Aux'
+// <Aux></Aux> is same as <Fragment></Fragment> if you import the named component { Fragment } from 'react' as well.
+
+
+const layout = ( props ) => (
+    <Aux><div>Toolbar, SideDrawer, Backdrop</div>
+    <main> 
+        {props.children}
+    </main>
+    </Aux>
+)
+
+export default layout
